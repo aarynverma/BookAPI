cee95b7c486ca22d378f8016dd0d7fb10e4cdbe2
aarynverma <18100BTCSE02667@svvvindore.onmicrosoft.com>
2022-08-19 16:17:45 +0530

    Add project files.

---
+++ BookAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookAPI.Models;
+
+namespace BookAPI.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+        Task<Book> Get(int id);
+        Task<Book> Create(Book Book);
+        Task Update(Book Book);
+        Task Delete(int id);
+    }
+}
